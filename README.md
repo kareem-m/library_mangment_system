5ce9049eb6d983d80eaa4fbaba5c2fb1d7ae82ba
Kareem Mohamed <kareemmoh1911@gmial.com>
2024-12-18 15:01:47 +0200

    LMS

---
+++ README.md
@@ -0,0 +1,3 @@
+# Library management system (LMS) project for FCDS
+
+The project was implemented in <b>Java programming language</b> according to the standards of this <a href="Final_Project.pdf">file</a>.
--- src/Main.java
+++ src/Main.java
@@ -3,140 +3,241 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter the number of books you have: ");
-        int bookCount = sc.nextInt();
+        System.out.println("Welcome to the Library Management System!");
+        System.out.print("Enter the maximum number of books the library can store: ");
+        int maxBooks = sc.nextInt();
+        sc.nextLine();
 
-        String[] bookTitles = new String[bookCount];
-        String[] bookDescriptions = new String[bookCount];
-        boolean[] bookIssued = new boolean[bookCount];
+        String[] bookTitles = new String[maxBooks];
+        String[] bookDescriptions = new String[maxBooks];
+        boolean[] bookIssued = new boolean[maxBooks];
 
-        for (int i = 0; i < bookCount; i++) {
-            System.out.print("Enter the title of book " + (i + 1) + ": ");
-            bookTitles[i] = sc.next();
+        int bookCount = 0;
 
-            System.out.print("Enter the description of book " + (i + 1) + ": ");
-            bookDescriptions[i] = sc.next();
+        boolean app = true;
+        while (app) {
+            System.out.println("\nMenu:");
+            System.out.println("1. Add a book");
+            System.out.println("2. Search for a book (by title or ID)");
+            System.out.println("3. Issue a book (by ID)");
+            System.out.println("4. Return a book (by ID)");
+            System.out.println("5. Delete a book (by ID)");
+            System.out.println("6. Edit book details (title/description by ID)");
+            System.out.println("7. View all books");
+            System.out.println("8. Exit");
 
-            bookIssued[i] = false; // available
-        }
+            System.out.print("\nEnter your choice: ");
+            int choice = sc.nextInt();
+            sc.nextLine();
 
-        System.out.println("Welcome to the library management System!");
+            switch (choice) {
+                case 1: // Add a book
+                    if (bookCount < maxBooks) {
+                        System.out.print("Enter the title of the book: ");
+                        String newTitle = sc.nextLine();
 
-        System.out.println("1. Add a book");
-        System.out.println("2. Search for a book (by title or ID)");
-        System.out.println("3. Isseue a book (by ID)");
-        System.out.println("4. Return a book (by ID)");
-        System.out.println("5. Delete a book (by ID)");
-        System.out.println("6. Edit book detailes (title / description by ID)");
-        System.out.println("7. View all books");
-        System.out.println("8. Exit");
-
-        System.out.println("Enter your choice");
-        int choice = sc.nextInt();
-
-        switch (choice) {
-            case 1: // add
-                bookCount++;
-                String[] newBookTitles = new String[bookCount];
-                String[] newBookDescriptions = new String[bookCount];
-                boolean[] newBookIssued = new boolean[bookCount];
-                for (int i = 0; i < bookCount; i++) {
-                    if (i < (bookCount - 1)) {
-                        newBookTitles[i] = bookTitles[i];
-                        newBookDescriptions[i] = bookDescriptions[i];
-                        newBookIssued[i] = bookIssued[i];
-                    } else if (i == (bookCount - 1)) {
-                        System.out.print("Enter the title of book " + (i + 1) + ": ");
-                        newBookTitles[i] = sc.next();
-
-                        System.out.print("Enter the description of book " + (i + 1) + ": ");
-                        newBookDescriptions[i] = sc.next();
-
-                        newBookIssued[i] = false;
-                    }
-                    System.out.println(newBookTitles[i]);
-                }
-                break;
-            case 2: // search
-                System.out.print("Search using title or ID ? ");
-                String titleID = sc.next();
-                if (titleID.equals("title")) {
-                    System.out.print("Enter the book title: ");
-                    String title = sc.next();
-                    for (int i = 0; i < bookCount; i++) {
-                        if (bookTitles[i].equals(title)) {
-                            System.out.println(bookTitles[i]);
-                            System.out.println(bookDescriptions[i]);
-                            if (bookIssued[i] == false) {
-                                System.out.println("available");
-                            } else if (bookIssued[i] == true) {
-                                System.out.println("issued");
+                        if (newTitle.isEmpty()) {
+                            while (newTitle.isEmpty()) {
+                                System.out.println("The title can't be empty");
+                                System.out.print("Enter the title of the book: ");
+                                newTitle = sc.nextLine();
                             }
                         }
-                    }
-                } else if (titleID.equals("ID")) {
-                    int ID = sc.nextInt();
-                    System.out.println(bookTitles[ID]);
-                    System.out.println(bookDescriptions[ID]);
-                    if (bookIssued[ID] == false) {
-                        System.out.println("available");
-                    } else if (bookIssued[ID] == true) {
-                        System.out.println("issued");
-                    }
-                }
-                break;
-            case 3: // isseue
-                System.out.print("Enter the ID: ");
-                int ID = sc.nextInt();
-                if (bookIssued[ID] == false) {
-                    System.out.println("the book " + bookTitles[ID] + " is available and you can borrow it");
-                    bookIssued[ID] = true;
-                } else if (bookIssued[ID] == true) {
-                    System.out.print("The book is already unavailable");
-                }
-                break;
-            case 4: // return
-                System.out.print("Enter the ID: ");
-                ID = sc.nextInt();
-                if (bookIssued[ID] == true) {
-                    System.out.println("the book " + bookTitles[ID] + " is now available and you can borrow it");
-                    bookIssued[ID] = true;
-                } else if (bookIssued[ID] == false) {
-                    System.out.print("The book is already available");
-                }
-                break;
-            case 5: // delete
-                break;
-            case 6: // edit
-                System.out.print("Enter the ID of the book that you need to edit: ");
-                ID = sc.nextInt();
 
-                System.out.print("Enter the new name of the book: ");
-                bookTitles[ID] = sc.next();
+                        System.out.print("Enter the description of the book: ");
+                        String newDescription = sc.nextLine();
 
-                System.out.print("Enter the new description of the book: ");
-                bookDescriptions[ID] = sc.next();
+                        bookTitles[bookCount] = newTitle;
+                        bookDescriptions[bookCount] = newDescription;
+                        bookIssued[bookCount] = false; // Available
 
-                System.out.println("The new title is " + bookTitles[ID] + " and the new description is " + bookDescriptions[ID]);
-                break;
-            case 7: // View
-                for (int i = 0; i < bookCount; i++) {
-                    System.out.println("The name of book is " + bookTitles[i]);
-                    System.out.println("The description of book is " + bookDescriptions[i]);
-                    if (bookIssued[i] == false) {
-                        System.out.println("available");
-                    } else if (bookIssued[i] == true) {
-                        System.out.println("issued");
+                        bookCount++;
+                        System.out.println("Book added successfully!");
+
+                        System.out.println("\nThe books in your library now :");
+                        for (int i = 0; i < bookCount; i++) {
+                            System.out.println("Book ID: " + (i + 1));
+                            System.out.println("Title: " + bookTitles[i]);
+                            System.out.println("Description: " + bookDescriptions[i]);
+                            if (bookIssued[i] == false) {
+                                System.out.println("Status: Available\n");
+                            } else if (bookIssued[i] == true) {
+                                System.out.println("Status: Issued\n");
+                            }
+                        }
+
+                    } else {
+                        System.out.println("Library is full! Cannot add more books");
                     }
-                }
-                break;
-            case 8: // exit
-                sc.close();
-                break;
-            default:
-                System.out.println("Invalid input");
-                break;
+                    break;
+
+                case 2: // Search for a book
+                    if (bookCount == 0) {
+                        System.out.println("No books in the library to search.");
+                        break;
+                    }
+
+                    System.out.print("Search by (title/ID): ");
+                    String searchType = sc.nextLine();
+
+                    if (searchType.equals("title")) {
+                        System.out.print("Enter the book title: ");
+                        String title = sc.nextLine();
+                        for (int i = 0; i < bookCount; i++) {
+                            if (bookTitles[i].equals(title)) {
+                                System.out.println("\nBook ID: " + (i + 1));
+                                System.out.println("Title: " + bookTitles[i]);
+                                System.out.println("Description: " + bookDescriptions[i]);
+                                if (bookIssued[i] == false) {
+                                    System.out.println("Status: Available");
+                                } else if (bookIssued[i] == true) {
+                                    System.out.println("Status: Issued");
+                                }
+                            } else {
+                                System.out.println("No book in the library with this name");
+                                break;
+                            }
+                        }
+                    } else if (searchType.equals("ID") || searchType.equals("id")) {
+                        System.out.print("Enter the book ID: ");
+                        int id = sc.nextInt();
+                        id--;
+                        sc.nextLine();
+                        if (id >= 0 && id < bookCount) {
+                            System.out.println("\nTitle: " + bookTitles[id]);
+                            System.out.println("Description: " + bookDescriptions[id]);
+                            if (bookIssued[id] == false) {
+                                System.out.println("Status: Available");
+                            } else if (bookIssued[id] == true) {
+                                System.out.println("Status: Issued");
+                            }
+                        } else {
+                            System.out.println("Invalid ID.");
+                        }
+                    } else {
+                        System.out.println("Invalid search type.");
+                    }
+                    break;
+
+                case 3: // Issue a book
+                    if (bookCount == 0) {
+                        System.out.println("No books in the library to issue.");
+                        break;
+                    }
+
+                    System.out.print("Enter the book ID to issue: ");
+                    int issueID = sc.nextInt();
+                    sc.nextLine();
+                    issueID--;
+
+                    if (issueID >= 0 && issueID < bookCount) {
+                        if (bookIssued[issueID] == false) {
+                            bookIssued[issueID] = true;
+                            System.out.println("You have issued the book: " + bookTitles[issueID]);
+                        } else {
+                            System.out.println("Book is already issued.");
+                        }
+                    } else {
+                        System.out.println("Invalid ID.");
+                    }
+                    break;
+
+                case 4: // Return a book
+                    if (bookCount == 0) {
+                        System.out.println("No books in the library to return.");
+                        break;
+                    }
+
+                    System.out.print("Enter the book ID to return: ");
+                    int returnID = sc.nextInt();
+                    sc.nextLine();
+                    returnID--;
+
+                    if (returnID >= 0 && returnID < bookCount) {
+                        if (bookIssued[returnID] == true) {
+                            bookIssued[returnID] = false;
+                            System.out.println("You have returned the book: " + bookTitles[returnID]);
+                        } else {
+                            System.out.println("Book was not issued.");
+                        }
+                    } else {
+                        System.out.println("Invalid ID.");
+                    }
+                    break;
+
+                case 5: // Delete a book
+                    if (bookCount == 0) {
+                        System.out.println("No books in the library to delete.");
+                        break;
+                    }
+
+                    System.out.print("Enter book ID to delete: ");
+                    int deleteId = sc.nextInt();
+                    sc.nextLine();
+                    deleteId--;
+
+                    if (deleteId >= 0 && deleteId < bookCount) {
+                        for (int i = deleteId; i < bookCount - 1; i++) {
+                            bookTitles[i] = bookTitles[i + 1];
+                            bookDescriptions[i] = bookDescriptions[i + 1];
+                            bookIssued[i] = bookIssued[i + 1];
+                        }
+                        bookCount--;
+                        System.out.println("Book deleted successfully!");
+                    } else {
+                        System.out.println("Invalid ID.");
+                    }
+                    break;
+
+                case 6: // Edit a book
+                    if (bookCount == 0) {
+                        System.out.println("No books in the library to edit.");
+                        break;
+                    }
+
+                    System.out.print("Enter the book ID to edit: ");
+                    int editID = sc.nextInt();
+                    sc.nextLine();
+                    editID--;
+
+                    if (editID >= 0 && editID < bookCount) {
+                        System.out.print("Enter the new title: ");
+                        bookTitles[editID] = sc.nextLine();
+                        System.out.print("Enter the new description: ");
+                        bookDescriptions[editID] = sc.nextLine();
+                        System.out.println("Book details updated.");
+                    } else {
+                        System.out.println("Invalid ID.");
+                    }
+                    break;
+
+                case 7: // View all books
+                    if (bookCount == 0) {
+                        System.out.println("No books in the library.");
+                        break;
+                    }
+
+                    for (int i = 0; i < bookCount; i++) {
+                        System.out.println("Book ID: " + (i + 1));
+                        System.out.println("Title: " + bookTitles[i]);
+                        System.out.println("Description: " + bookDescriptions[i]);
+                        if (bookIssued[i] == false) {
+                            System.out.println("Status: Available\n");
+                        } else if (bookIssued[i] == true) {
+                            System.out.println("Status: Issued\n");
+                        }
+                    }
+                    break;
+
+                case 8: // Exit
+                    System.out.println("Thank you for using the Library Management System");
+                    app = false;
+                    break;
+
+                default:
+                    System.out.println("Invalid choice.");
+                    break;
+            }
         }
-
     }
 }
